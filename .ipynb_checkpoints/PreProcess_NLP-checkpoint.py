ef84e8ac4ab3d144e6b86bb1330db54d0a244c41
Jana Albert <s16af9@umwelt-campus.de>
2022-11-17 14:52:44 +0100

    'update_code'

--- .ipynb_checkpoints/PreProcess_NLP-checkpoint.py
+++ .ipynb_checkpoints/PreProcess_NLP-checkpoint.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[28]:
 
 
 import re
 import fitz
 import spacy
-import requests
-import IPython
+#import requests
+#import IPython
 import pathlib
 import nltk
 import pandas as pd
 import numpy as np
 #nltk.download('wordnet')
 #nltk.download('stopwords')
-from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-from nltk.tokenize import RegexpTokenizer, word_tokenize
+#from nltk.stem.porter import PorterStemmer
 from nltk.tokenize.treebank import TreebankWordDetokenizer
+spacy.load('en_core_web_sm')
+from nltk.tokenize import RegexpTokenizer, word_tokenize
+from keyphrase_vectorizers import KeyphraseCountVectorizer
 from keybert import KeyBERT
+from sentence_transformers import SentenceTransformer
 #nltk.download('wordnet') 
-from nltk.stem.wordnet import WordNetLemmatizer
+#from nltk.stem.wordnet import WordNetLemmatizer
 
 
-# In[4]:
+# In[18]:
 
 
 def build_corpus(files_path):
-    text_list = {'title': [],
+    text_dict = {'title': [],
                  'text': []}
 
     #Iteration über PDF's und in text speichern. Alle PDF's liegen in text_list Es werden nur die ersten beiden Seiten eingelesen
@@ -54,29 +56,38 @@ def build_corpus(files_path):
         if not has_abstract:
             text = text_tmp
 
-        text_list['title'].append(cur_title)
-        text_list['text'].append(text)
+        text_dict['title'].append(cur_title)
+        text_dict['text'].append(text)
         
 
         cur_reader.close()
         
-    return text_list
+    return text_dict
 
 
-# In[5]:
+# In[19]:
 
 
-def get_stopwords():
-#Englische Stop Words
-    stop_words = set(stopwords.words("english"))
-#Hinzufügen eigener Stopwords
-    new_words = ["the", "as", "was", "that", "open", "access", "thought", "sees", "agreement", "term", "initially", "people", "eu", "citiations", "de", "authors",
-                "com", "citations", "table", "et", "al", "conference", "th", "ieee", "fig", "aaai", "www", "org", "yet", "http" ]
-    my_stop_words = stop_words.union(new_words)
-    return my_stop_words
+def build_conference(files_path):
+    con_dict = {'title': [],
+                 'text': []}
+    
+    for file in files_path:
+        cur_reader = fitz.open(file)
+        text = ''
+        cur_title = cur_reader.metadata['title']
+        
+        for i in range(len(cur_reader)):  
+            page = cur_reader.load_page(i)
+            text +=  cur_reader.get_page_text(i)
+            
+        con_dict['title'].append(cur_title)
+        con_dict['text'].append(text)
+        
+    return con_dict
 
 
-# In[6]:
+# In[20]:
 
 
 def preprocess_corpus(corpus):
@@ -93,189 +104,102 @@ def preprocess_corpus(corpus):
         
     detokenize = TreebankWordDetokenizer().detokenize(all_tokens)
     corpus['text'].append(detokenize)
+    
     return corpus
+
+
+# In[21]:
+
+
+def get_stopwords():
+#Englische Stop Words
+    stop_words = set(stopwords.words("english"))
+#Hinzufügen eigener Stopwords
+    new_words = ["the", "as", "was", "that", "open", "access", "thought", "sees", "agreement", "term", "initially", "people", "eu", "citiations", "de", "authors",
+                "com", "citations", "table", "et", "al", "conference", "th", "ieee", "fig", "aaai", "www", "org", "yet", "http","open access" ]
+    my_stop_words = stop_words.union(new_words)
+    
+    return my_stop_words
+
+
+# In[26]:
+
+
+def generate_keywords(corpus, conference_corpus):
+    
+    corpus = preprocess_corpus(corpus)
+    
+    paper_keys = {'keywords': [], 
+                'relevance':[],
+                'paper': [] }
+    
+    conference_keys = {'keywords': [], 
+                        'relevance':[],
+                        'conference': [] }
+    
+    sentence_model = SentenceTransformer("all-mpnet-base-v2")
+    kw_model = KeyBERT(model=sentence_model)
+    vectorizer = KeyphraseCountVectorizer(stop_words=get_stopwords())
+    
+
+    #iteration über alle PDF's
+
+    for x in range(len(corpus['title'])): 
+        doc_embedding, word_embedding = kw_model.extract_embeddings(corpus['text'][x], vectorizer=vectorizer)
+        keywords_dist_paper = kw_model.extract_keywords(corpus['text'][x], vectorizer=vectorizer, use_mmr=True, diversity=0.6, 
+                                                        top_n=20, doc_embeddings=doc_embedding, 
+                                                        word_embeddings=word_embedding)
+
+        
+        for i in range(len(keywords_dist_paper)):
+            keyword =  keywords_dist_paper[i][0]
+            paper_keys['keywords'].append(keyword)
+            relevance = keywords_dist_paper[i][1]
+            paper_keys['relevance'].append(relevance)
+            paper_keys['paper'].append(corpus["title"][x])
+            
+    for x in range(len(conference_corpus['title'])): 
+        doc_embedding, word_embedding = kw_model.extract_embeddings(conference_corpus['text'][x], vectorizer=vectorizer)
+        keywords_dist_conference = kw_model.extract_keywords(corpus['text'][x], vectorizer=vectorizer, use_mmr=True, diversity=0.5, 
+                                                        top_n=20, doc_embeddings=doc_embedding, 
+                                                        word_embeddings=word_embedding)
+        
+        for i in range(len(keywords_dist_conference)):
+            keyword =  keywords_dist_conference[i][0]
+            conference_keys['keywords'].append(keyword)
+            relevance = keywords_dist_conference[i][1]
+            conference_keys['relevance'].append(relevance)
+            conference_keys['conference'].append(conference_corpus["title"][x])
+            
+        #Umwandlung in Dataframe
+
+    df_paper = pd.DataFrame(paper_keys)
+    filepath_paper = 'keywords/paper_Keywords.csv'
+    df_paper = df_paper.sort_values('relevance',ascending=False)
+    df_paper.to_csv(filepath_paper)
+
+    df_conference = pd.DataFrame(conference_keys)
+    filepath_conference = 'keywords/conference_Keywords.csv'
+    df_conference = df_conference.sort_values('relevance',ascending=False)
+    df_conference.to_csv(filepath_conference)
+
+    return paper_keys, conference_keys
 
 
 # In[11]:
 
 
-def generate_keywords(corpus):
-    
-    corpus = preprocess_corpus(corpus)
-    df_keys = {'keywords': [], 
-                'relevance':[],
-                'paper': [],
-                'conference': []}
-    keywords = []
-    kw_model = KeyBERT()
-    vectorizer = CountVectorizer(ngram_range=(1,3), stop_words=get_stopwords())
-
-    
-    #Erstellt mir ngrams vom Text, sind auch keywords aus n-grammen. Das vielleicht als Attribut an die paper? bläht Graph nicht auf und danach kann dann gesucht werden? 
-    #ToDo: Funktion weiter untersuchen und vielleicht an Parametern basteln
-    #iteration über alle PDF's
-    
+new_words = ["the", "as", "was", "that", "open", "access", 
+               "thought", "sees", "agreement", "term", "initially",
+               "people", "eu", "citiations", "de", "authors",
+               "com", "citations", "table", "et", "al", "conference", 
+               "th", "ieee", "fig", "aaai", "www", "org", "yet", "http", 'open access' ]
+print(stopwords.words('english'))
 
 
-    for x in range(len(corpus['title'])): 
-
-        keywords_dist = kw_model.extract_keywords(corpus['text'][x], vectorizer=vectorizer, top_n=20, use_maxsum=True, nr_candidates=10, use_mmr=True, diversity=0.5)
-        for i in range(len(keywords_dist)):
-            keyword =  keywords_dist[i][0]
-            df_keys['keywords'].append(keyword)
-            relevance = keywords_dist[i][1]
-            df_keys['relevance'].append(relevance)
-            df_keys['paper'].append(corpus["title"][x])
-            df_keys['conference'].append(["conferenceTitel"])
-            
-        #Umwandlung in Dataframe
-
-    df = pd.DataFrame(df_keys)
-    filepath = 'keywords/Test_Keywords.csv'
-
-    df = df.sort_values('relevance',ascending=False)
-    df.to_csv(filepath) #Erstellung csv pro Text   
-
-        #keywords csv's sollen in Variable geschrieben werden (Liste)
-    return df_keys
+# In[16]:
 
 
-# In[12]:
-
-
-#pdf_path = ('C:/Users/Jana/LabCode/PDF')
-#p = pathlib.Path(pdf_path)
-#files_path = list(p.glob('*.pdf'))
-#corpus = build_corpus(files_path)
-#print(type(corpus))
-#print(range(len(corpus['title'])))
-#print(corpus)
-
-
-#keyword_list = generate_keywords(corpus)
-
-
-# In[ ]:
-
-
-# Eventuell Pos_tagging, Dependency, n-Gram Bildung ect zur Verbesserung der Ergebnisse
-
-def preprocess():
-    corpus = []
-    keywords = []
-    #Herauszufilternde Satzzeichen
-    punctuations = ['(','.',')',',','[',']',';',':','_','*','']
-    #Lemma und Stemma
-    lem = WordNetLemmatizer()
-    ps= PorterStemmer() ##Stemming
-
-    #iteration über alle PDF's
-    for i in range(len(text_list)):
-        text = text_list[i] 
-        text = text.lower() #alles kleinschreiben
-        tokens = word_tokenize(text) #tokens auf Wortebene
-       #Iteration über alle tokens pro PDF
-        for i in range(len(tokens)):
-            tokens[i] = re.sub("&lt;/?.*?&gt;"," &lt;&gt; ", tokens[i]) #entfernt tags
-            tokens[i] = re.sub("(\\d|\\W)+","", tokens[i])   #entfernt Zahlen und spezielle Satzzeichen, toDo: Bindestrich hinbekommen
-            #Entfernen einzelner und 2 Buchstaben
-            if(len(tokens[i]) <= 2):
-               tokens[i] = ''
-
-        #tokens = [lem.lemmatize(word) for word in tokens if not word in stop_words] #Lemmatisation ist das eine gute Idee?
-        tokens = [ps.stem(word) for word in tokens if not word in stop_words] #Lemmatisation ist das eine gute Idee?
-
-        keywords.append([word for word in tokens if not word in stop_words and not word in punctuations]) #Erstellen keywords (preprocessed) ohne Satzzeichen und Stop Words ect.
-        corpus.append(text) # pre Processed Corpus -> gesamter Text in kleinbuchstaben
-
-
-# In[ ]:
-
-
-#brauch ich das noch?
-def word_count(text):
-    counts = dict()
-    words = text.split(" ")
-
-    #print(words)
-    for word in words:
-        if word in counts:
-            counts[word] += 1
-        else:
-            counts[word] = 1
-
-    return words, counts
-
-
-# In[ ]:
-
-
-def weightage(word,text,number_of_documents=1):
-    
-    number_of_times_word_appeared = len(word_list)
-
-    tf = number_of_times_word_appeared/float(len(text))
-
-    idf = np.log((number_of_documents)/float(number_of_times_word_appeared))
-    
-    tf_idf = tf*idf
-    
-    return number_of_times_word_appeared, tf, idf, tf_idf 
-
-
-# In[ ]:
-
-
-def word_appearance():
-
-    #def x(corpus, keywords): #wenn ich es als Funtkion mache bekomme ich andere Ausgabe. Aber wieso ?!
-    #Iteration über Texte
-    for i in range(len(corpus)):
-        df_keys = {'keywords': [], #Erstellung Dictionary für nachher DataFrame
-                  'number_of_times_word_appeared': [],
-                  'idf': [],
-                  'tf': [],
-                  'tf_idf': []}
-        for keyword in keywords[i]: #Iteration über keywords pro Text
-            word_list = re.findall(keyword,corpus[i]) #Finde die keywords wieder im Text
-            if len(word_list) >= 1: #Falls keyword gefunden
-                curArray = []
-                curArray = weightage(keyword, corpus[i])    
-
-                #if keyword not in df_keys.get('keywords') : #funktioniert so nicht
-                #Füllung des Dictionary anhand der Funktion weightage
-                df_keys['keywords'].append([keyword])
-                df_keys['number_of_times_word_appeared'].append(curArray[0])
-                df_keys['tf'].append(curArray[1])
-                df_keys['idf'].append(curArray[2])
-                df_keys['tf_idf'].append(curArray[3])
-    #Umwandlung in Dataframe
-        df = pd.DataFrame(df_keys)
-
-        df=df.loc[df.astype(str).drop_duplicates(subset='keywords').index]
-        df = df.sort_values('tf_idf',ascending=True)
-        df.head(25).to_csv(str(i)+'_Keywords.csv') #Erstellung csv pro Text
-    
-
-
-        
-     
-
-
-# In[ ]:
-
-
-def better_vectorizer():
-    cv=CountVectorizer(max_df=3, min_df=0, stop_words=stop_words, max_features=10000, ngram_range=(1,3))
-    fittedStopwords = ["dummy" for dummy in range(len(corpus))]
-    for i in range(len(corpus)):
-        corpus[i] = [corpus[i]]
-        fittedStopwords[i] = cv.fit_transform(corpus[i])
-    print(fittedStopwords)
-
-
-# In[ ]:
-
-
-
+vectorizer = KeyphraseCountVectorizer(stop_words=get_stopwords())
+print(vectorizer.get_params())
 
--- PreProcessMetadata.py
+++ PreProcessMetadata.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[14]:
 
 
 from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -9,17 +9,17 @@ import fitz
 import os
 import pathlib
 import pandas as pd
-import numpy as np
+#import numpy as np
 
 
 # In[2]:
 
 
-# initialerstellung der csv mit allen original PDF's
+# initialerstellung der csv mit allen PDF's
 def read_paper_meta(path):
     paper_path = str(path) + "PDF"
-    p = pathlib.Path(path)
-    pp = pathlib.Path(paper_path)
+    #p = pathlib.Path(path)
+    p = pathlib.Path(paper_path)
     pdf_names = []
     meta_author = []
     meta_title = []
@@ -27,18 +27,17 @@ def read_paper_meta(path):
     meta_conference = []
     meta_check = []
     meta_list = []
-    files_path = list(pp.glob('*.pdf'))
+    files_path = list(p.glob('*.pdf'))
     is_checked = True
-    
     
     #Itertion über alle files
     for file in files_path:
-        meta_control = pd.read_excel(str(path) + 'metadata_list.xlsx') # was wenn Datei noch nicht existiert?
+        meta_control = pd.read_excel(str(path) + 'metadata_list.xlsx')
         cur_reader = PdfFileReader(file)
         cur_pdf_name = pathlib.Path(file).name #holen PDF Name   
-        
+        #Prüfung ob PDF bereits in metadata_list enthalten
         if(cur_pdf_name in meta_control.values):
-            cur_index = meta_control.loc[meta_control['PDF'] == cur_pdf_name]
+            cur_index = meta_control.loc[meta_control['PDF'] == cur_pdf_name] #Zeile der PDF
             if(pd.isna(cur_index.Check.iloc[0])): #wenn Check bei der PDF leer, dann check auf False
                 is_checked = False
         #Falls name der Datei nicht in der Excel, hole die Metadaten und speicher diese weg     
@@ -49,7 +48,9 @@ def read_paper_meta(path):
             cur_writer.appendPagesFromReader(cur_reader)
             cur_writer.addMetadata(cur_metadata)
             pdf_names.append(cur_pdf_name)
-            
+            #Prüfung für jede Metadaten Werte, ob diese überhaupt existieren
+            #Falls nicht, leer anlegen
+            #Falls ja, in metadata_list reinschreiben
             if('/Title' in cur_metadata.keys()):
                 meta_title.append(cur_metadata.title)
             else:
@@ -91,7 +92,6 @@ def read_paper_meta(path):
                 meta_conference.append(cur_metadata['/ConferenceName'])    
                 
             meta_check.append('')
-
             #Erstellung Dictionary mit den Metadaten der PDF's
             meta_list = {'PDF': pdf_names,
                         'Titles': meta_title,
@@ -103,53 +103,47 @@ def read_paper_meta(path):
             df_meta = pd.DataFrame(meta_list)
             #Zusammenfügen der bereits existierenden Datei und den neuen Metadaten
             meta_control = pd.concat([meta_control, df_meta])
-            #Ausgabe Excel
+
+    #Ausgabe Excel
     meta_control.to_excel(str(path) + 'metadata_list.xlsx', index=False, header=True)     
-            #Öffnen der Excel falls unvollständig
-            #os.system("start EXCEL.EXE " + str(path) + "metadata_list.xlsx")
-            #raise Exception('Anpassen der Metadaten wegen neuer Datei notwendig. Datei wird geöffnet.\n\t\tAnschließend Programm neustarten.')
-    #weitere Prüfung auf Check, dann auch öffnen der Datei   
-    if(not is_checked):
+    if(not is_checked): #Prüfung auf Check, dann auch öffnen der Datei   
         os.system("start EXCEL.EXE " + str(path) + "metadata_list.xlsx")
         raise Exception('Anpassen der Metadaten in Datei notwendig. Datei wird geöffnet.\n\t\tAnschließend Programm neustarten.')
     
     return(meta_control)
 
 
-# In[ ]:
+# In[1]:
 
 
-# Anpassung der Metadaten in den eigentlichen PDF's:
-#hier auch Anpassen der Metadaten eines aim&scope, da soll der Name der Konferenz noch rein. Über match?
-def write_paper_meta(path):
+#Anpassung der Metadaten in den eigentlichen PDF's:
+def write_paper_meta(path, pdf_meta):
     paper_path = str(path) + "PDF/"
-    #Einlesen der Excel
-    excel_meta = pd.read_excel(str(path) + '/metadata_list.xlsx')
     
-    #Iteration über jeden Eintrag und Veränderung der Metadaten für jeweilige PDF anhand der Einträge in Excel
-    for x in range(len(excel_meta)):
-        cur_pdf = excel_meta['PDF'][x] #Name der PDF  
+    #Iteration über jeden Eintrag und Veränderung der Metadaten 
+    #für jeweilige PDF anhand der Einträge pdf_meta
+    for x in range(len(pdf_meta)):
+        cur_pdf = pdf_meta['PDF'][x] #Name der PDF  
         cur_pdf_full = paper_path + cur_pdf #voller Pfad der jeweiligen PDF
         cur_reader = PdfFileReader(cur_pdf_full) #aktuelle PDF lesen können
         cur_writer = PdfFileWriter() # aktuelle PDF schreiben können
         cur_writer.appendPagesFromReader(cur_reader)
         cur_metadata = cur_reader.getDocumentInfo() #Metadaten der aktuellen PDF holen
         cur_writer.addMetadata(cur_metadata) #Metadaten der aktuellen PDF ändern
-        cur_writer.addMetadata({"/Title": excel_meta['Titles'][x]})
-        cur_writer.addMetadata({"/Author": excel_meta['Authors'][x]})
+        cur_writer.addMetadata({"/Title": pdf_meta['Titles'][x]})
+        cur_writer.addMetadata({"/Author": pdf_meta['Authors'][x]})
         #custom Metadata
-        cur_writer.addMetadata({"/CreationDate": str(excel_meta['Date'][x])})
-        cur_writer.addMetadata({"/ConferenceName": excel_meta['Conference'][x]})
+        cur_writer.addMetadata({"/CreationDate": str(pdf_meta['Date'][x])})
+        cur_writer.addMetadata({"/ConferenceName": pdf_meta['Conference'][x]})
         with open(cur_pdf_full, "wb") as fp:
             cur_writer.write(fp)
     
 
 
-# In[1]:
+# In[2]:
 
 
 def write_conference_meta(path, pdf_meta):
-    
     
     conference_path = str(path) + "/AimsAndScope"
     p_con = pathlib.Path(conference_path)
@@ -162,8 +156,8 @@ def write_conference_meta(path, pdf_meta):
             text = ''
             for page in cur_reader:  
                 text += (page.get_text("text"))
-            if cur_conference.lower() in text.lower(): #funktioniert noch nicht so ganz wenn man im Text sucht, aber wenn man über PDF Namen geht schon. Was ist besser?
-                cur_reader = PdfFileReader(file) #aktuelle PDF lesen können'
+            if cur_conference in text:
+                cur_reader = PdfFileReader(file) #aktuelle PDF lesen können
                 cur_writer = PdfFileWriter() # aktuelle PDF schreiben können
                 cur_writer.appendPagesFromReader(cur_reader)
                 cur_metadata = cur_reader.getDocumentInfo() #Metadaten der aktuellen PDF holen
